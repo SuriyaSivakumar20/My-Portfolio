cabed2aeca8c7f57df1a99e2707f1791b38c9321
SuriyaSivakumar20 <suriyasivakumar08@gmail.com>
2025-09-22 10:12:54 +0530

    Initial commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -42,10 +42,10 @@ const About: React.FC = () => {
             >
               <div className="aspect-square w-full max-w-md mx-auto overflow-hidden rounded-2xl neon-border">
                 <img 
-                  src="su.jpg" 
-                  alt="Suriya Sivakumar" 
-                  className="w-full h-full object-cover"
-                />
+                    src="su.jpg" 
+                    alt="Suriya Sivakumar" 
+                    className="w-full h-full object-cover"
+/>
               </div>
               
               <motion.div 
